a715d7c53b2f62ceabe65385a6fc2c8e719a72ad
saksham gupta <abcdsaksham1234@gmail.com>
2026-08-13 14:23:49 +0530

    Add greet and addNumbers functions with usage

--- lesson_1/functions.js
+++ lesson_1/functions.js
@@ -1,1 +1,12 @@
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
 
+function addNumbers(a, b) {
+    return a + b;
+}
+
+greet("Saksham");
+
+let result = addNumbers(10, 20);
+console.log("The sum is:", result);
