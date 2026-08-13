bd42a13cfafb8301278c78a77f6a34ba8b5b8eab
saksham gupta <abcdsaksham1234@gmail.com>
2026-08-13 14:17:26 +0530

    Add basic arithmetic operations

--- lesson_1/arithmatic_ka_chod.js
+++ lesson_1/arithmatic_ka_chod.js
@@ -1,4 +1,7 @@
-console.log((10-3)*2);
-let x=4;
-let y=8;
-console.log(x>y);
+let a = 10;
+let b = 5;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
